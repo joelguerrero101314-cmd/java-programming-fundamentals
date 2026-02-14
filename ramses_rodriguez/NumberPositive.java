e24286464ef9df32cec91f909db331327927f331
rramses390 <rramses390@gmail.com>
2026-02-13 21:14:35 -0500

    se extendioi el codigo hasta poder interactuar con el cliente donde se le solicita un numero

--- ramses_rodriguez/NumberPositive.java
+++ ramses_rodriguez/NumberPositive.java
@@ -1,7 +1,17 @@
+package ramses_rodriguez;
+import java.util.Scanner;
+
 public class NumberPositive{
 
     public static void main(String[] args){
-        
+        Scanner keyboard = new Scanner(System.in);
+       
+
+        System.out.println("Q xopa bloc indicame un numero entero");
+        System.out.print("Digita el numero: ");
+        String num = keyboard.nextLine();
+
+        if ()
     }
 }
 
