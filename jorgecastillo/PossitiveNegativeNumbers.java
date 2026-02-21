e3878abdea6a39eccf39df7a13bbec4e13af3bd4
Smack1678 <jorge.castillo8221@itse.ac.pa>
2026-02-21 18:22:47 -0500

    combios en print else final

--- jorgecastillo/PossitiveNegativeNumbers.java
+++ jorgecastillo/PossitiveNegativeNumbers.java
@@ -1,20 +1,21 @@
 package jorgecastillo;
+
 import java.util.Scanner;
 
 public class PossitiveNegativeNumbers {
-    
-   public static void main(String[] args) {
-    Scanner read = new Scanner(System.in);
 
-    System.out.print("Hola introduce un Numero y te dire si es positivo o negativo: ");
-    Integer numero = read.nextInt();
-    
-    if (numero > 0)
-        System.out.println("EL NUMERO ES POSITIVO");
-    else if (numero <0)
-        System.out.println("El numero es negativo");
-    else
-        System.out.println("El numero es igual a cero");
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
 
-   }
+        System.out.print("Hola introduce un Numero y te dire si es positivo o negativo: ");
+        Integer numero = read.nextInt();
+
+        if (numero > 0)
+            System.out.println("EL NUMERO ES POSITIVO");
+        else if (numero < 0)
+            System.out.println("El numero es negativo");
+        else
+            System.out.println("El numero es igual a cero y mas nada");
+
+    }
 }
