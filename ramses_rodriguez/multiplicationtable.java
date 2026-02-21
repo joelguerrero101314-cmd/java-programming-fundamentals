1b7536451c5067a90d9914f9e6f1df6284f423a9
rramses390 <rramses390@gmail.com>
2026-02-20 20:27:04 -0500

    formateo del codigo

--- ramses_rodriguez/multiplicationtable.java
+++ ramses_rodriguez/multiplicationtable.java
@@ -1,6 +1,7 @@
 package ramses_rodriguez;
 
 import java.util.Scanner;
+
 public class multiplicationtable {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
@@ -9,10 +10,10 @@ public class multiplicationtable {
         System.out.println("dale un limite a la tabla");
         int limit = keyboard.nextInt();
 
-System.out.println("\n -- tabla del " + number + " --");
-      
-        for (int i = 1; i <= limit; i++){
-        System.out.println(number + "x" + i + "=" + (number * i ) );
+        System.out.println("\n -- tabla del " + number + " --");
+
+        for (int i = 1; i <= limit; i++) {
+            System.out.println(number + "x" + i + "=" + (number * i));
         }
     }
 }
