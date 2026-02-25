fff545c692b53172ec2eba8b813043bc41a6317c
moises <moisesguerrero474@gmail.com>
2026-02-24 22:33:14 -0500

    programa que se repite mediante un ciclo repetitivo mientras el password ingresado sea incorrecto

---
+++ moises/ValidatePassword.java
@@ -0,0 +1,23 @@
+package moises;
+import java.util.Scanner;
+public class ValidatePassword {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+
+        String password;
+        do {
+
+            System.out.print("Ingrese el password correcto: ");
+            password = sc.next();
+
+            if (password.equals("Admin")) {
+                System.out.println("Password correcto");
+            }else{
+                System.out.println("Password incorrecto");
+            }
+
+        }while(! password.equals("Admin"));
+
+        sc.close();
+    }
+}
