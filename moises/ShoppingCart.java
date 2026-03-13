744aa709446a73b8392810475c02782495745fa9
moises <moisesguerrero474@gmail.com>
2026-03-13 16:14:27 -0500

    programa que simula un carrito de compras

---
+++ moises/ShoppingCart.java
@@ -0,0 +1,33 @@
+package moises;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ShoppingCart {
+    static ArrayList<Double> prices = new ArrayList<>();
+    static double sum = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("----------------------");
+        System.out.println("Carrito de compras..");
+        System.out.println("----------------------");
+
+        for(int i = 0; i < 5; i++) {
+            System.out.print("Precio " + (i + 1) + ": ");
+            prices.add(sc.nextDouble());
+        }
+
+        double sumPrices = calculateTotal(sc);
+
+        System.out.println("--------------------------");
+        System.out.println("Precio total: " + sumPrices);
+        System.out.println("--------------------------");
+        
+        sc.close();
+    }
+    static double calculateTotal(Scanner sc) {
+        for(double price : prices) {
+            sum += price;
+        }
+        return sum;
+    }
+}
